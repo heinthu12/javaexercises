e71ca93be61133f42ca5e1a7636e92637facf7f2
Hein Thu <44962128+heinthu12@users.noreply.github.com>
2020-01-24 15:28:30 +0900

    use time.printTime(time)

---
+++ 240112020/student/printStudent.java
@@ -0,0 +1,13 @@
+
+/**
+ * クラス printTime の注釈をここに書きます.
+ * 
+ * @author (Hein Thu)
+ * @version (24/01/2020)
+ */
+public class printStudent{
+    public static void main(String[] args){
+        Student time = new Student("Hein Thu",18,"heinthu6688@gmail.com","Myanmar");
+        time.printStudent(time); 
+    } 
+}
